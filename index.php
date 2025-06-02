f20270a669fd2e4e159f3a6572f153e788b6a039
Sanjeev Sharma <sanjeev.sharma@livepro.com>
2025-06-02 22:22:40 +1000

    duplicate index.html as index.php

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Vocals - Entertainment Booking</title>
+    <link rel="stylesheet" href="css/index.css">
+</head>
+<body>
+    <div id="header"></div>
+    <div id="global-spinner" class="global-spinner">
+        <div class="spinner-music"></div>
+    </div>
+    <main>
+        <div id="hero"></div>
+        <div id="intro"></div>
+        <div id="news"></div>
+        <div id="booking"></div>
+        <div id="login"></div>    
+        <div id="signup"></div>
+        <div id="content"></div>
+        <div id="register"></div>
+    </main>
+    <div id="footer"></div>
+    <script src="index.js"></script>
+    <script src="apiConfig.js"></script>
+</body>
+</html>
